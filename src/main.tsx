5049c0faadf1b284a7fa65929facf743d4614fc7
britney-brown <briit@tamu.edu>
2025-10-02 15:05:04 -0500

    update firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import './index.css'
 import App from './App.tsx'
@@ -9,7 +9,9 @@ createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
